8d386ea4c6b277c66f340c09f71302c531961e19
abspen1 <abspencer2097@gmail.com>
2020-12-19 23:11:11 -0700

    remove config

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,3 @@
-const { prefix } = require('../config.json');
-
 module.exports = {
 	name: 'help',
 	description: 'List all of my commands or info about a specific command.',
@@ -13,7 +11,7 @@ module.exports = {
 		if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
 			data.push(commands.map(command => command.name).join(', '));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+			data.push(`\nYou can send \`!help [command name]\` to get info on a specific command!`);
 
 			return message.author.send(data, { split: true })
 				.then(() => {
@@ -37,7 +35,7 @@ module.exports = {
 
 		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
 		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+		if (command.usage) data.push(`**Usage:** !${command.name} ${command.usage}`);
 
 		data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
 
