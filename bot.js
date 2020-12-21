14a4834547ea5c517f9a24a3b6adc90f7d956158
abspen1 <abspencer2097@gmail.com>
2020-12-21 14:50:56 -0700

    add profanity filter

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var Redis = require('ioredis')
+var badwordsArray = require('badwords/array');
 const fs = require('fs');
 const Discord = require('discord.js')
 const fetch = require('node-fetch')
@@ -41,7 +42,19 @@ redis.set("check-redis", "Redis is ready!");
 
 redis.get("check-redis").then((res) => console.log(res));
 
+redis.incr("test-incr")
+
+redis.get("test-incr").then((res) => console.log(res));
+
+/**
+ * When a message is sent in the server this function will be triggered
+ */
 client.on('message', message => {
+
+	/**
+	 * Before doing anything else I want to check the message for any bad words
+	 */
+	if (!message.author.bot) check_profanity(message);
 
     tokens = message.content.split(' ')
 
@@ -123,6 +136,70 @@ async function get_gif(message, tokens) {
     const index = Math.floor(Math.random() * json.results.length)
     message.channel.send(json.results[index].url)
     console.log("gif sent")
+}
+
+function check_profanity(message) {
+	for (var i in badwordsArray) {
+		if (message.content.toLowerCase().includes(badwordsArray[i])) {
+			let data = []
+			message.delete();
+			data.push("Profanity was found in your message:")
+			data.push(message.content)
+			let title = message.author + "-prof-count"
+			redis.incr(title)
+			let count = 0;
+			redis.get(title).then(function (res) {
+				count = res;
+				if (res > 2) {
+					/**
+					 * Uh oh this user can't control their potty mouth... 
+					 * I think for now this will just be a kick
+					 * The user can rejoin still if they have an invite and will be kicked again
+					 * each time they come back and send a profane message
+					 * 
+					 * Could adjust this to ban if need be
+					 * Also could have a second if that is 10 or so and have that ban the user
+					 */
+					member
+					.kick('User has sent a message with profanity at least 3 times now.')
+					.then(() => {
+						// We let the message author know we were able to kick the person
+						message.reply(`Successfully kicked ${user.tag}`);
+					})
+					.catch(err => {
+						// An error happened
+						// This is generally due to the bot not being able to kick the member,
+						// either due to missing permissions or role hierarchy
+						message.reply('I was unable to kick the member');
+						// Log the error
+						console.error(err);
+					});
+				}
+			});
+			let title = "You have now sent " + count + " messages marked as containing profanity.";
+			data.push(title)
+			if (3 - count == 1) {
+				title = "If you send 1 more profane message you will be kicked from the server.";
+			} else if (count > 2) {
+				title = "You have been kicked from the server for sending too many messages containing profanity.";
+			} else {
+				title = "If you send " + 3-count + " more profane messages you will be kicked from the server.";
+			}
+			data.push(title)
+			title = "If you believe to have received this in error and the message you sent was clean, ";
+			title += "please submit an appeal to one of the admins."
+			data.push(title)
+			return message.author.send(data, { split: true })
+				.then(() => {
+                    if (message.channel.type === 'dm') return;
+                    message.reply('https://tenor.com/view/watch-your-profanity-funny-gif-5600117');
+                })
+                .catch(error => {
+                    console.error(`Could not send profanity warning DM to ${message.author.tag}.\n`, error);
+                    message.reply('It seems like I can\'t DM you!\nhttps://tenor.com/view/watch-your-profanity-funny-gif-5600117');
+                });
+		}
+	}
 }
 
 
