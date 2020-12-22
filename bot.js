9b85b5afbff2553fe904546223bfd0171bfe01bf
abspen1 <abspencer2097@gmail.com>
2020-12-22 07:38:18 -0700

    log if channel isn't found

--- bot.js
+++ bot.js
@@ -150,7 +150,10 @@ client.on('guildMemberAdd', member => {
 	// Send the message to a designated channel on a server:
 	const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
 	// Do nothing if the channel wasn't found on this server
-	if (!channel) return;
+	if (!channel) {
+		console.log("Could not find channel to send welcome message")
+		return;
+	}
 	// Send the message, mentioning the member
 	channel.send(`Welcome to the server, ${member}`);
 });
