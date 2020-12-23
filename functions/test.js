67ad6e244e17c428a659662f4ba886583f53ef19
abspen1 <abspencer2097@gmail.com>
2020-12-23 14:22:40 -0700

    starting to add testing function

---
+++ functions/test.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "test",
+    aliases: ['testing', 'tests'],
+    description: "Tells you your permission level for the current message location.",
+    permLevel: "User",
+    test: true,
+    usage: '[all] OR [command name]'
+};
