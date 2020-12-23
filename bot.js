87c6321381a6b5d9dca7f68deccd6d559959ca43
abspen1 <abspencer2097@gmail.com>
2020-12-22 17:56:16 -0700

    purposeful error

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
 var Redis = require('ioredis')
 const fs = require('fs');
 const Discord = require('discord.js')
-
+func
+funx ksk;
 const client = new Discord.Client()
 
 client.commands = new Discord.Collection();
