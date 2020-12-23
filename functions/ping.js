69a75852de74c728b1fd29138881f3ee390acc09
abspen1 <abspencer2097@gmail.com>
2020-12-23 00:30:04 -0700

    switch ping to function

---
+++ functions/ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'ping',
+	description: 'Simple check for if the bot is functioning',
+	cooldown: 5,
+};
+
+module.exports.execute = async (client, redis, message) => { // eslint-disable-line no-unused-vars
+  const msg = await message.channel.send("Ping?");
+  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+};
