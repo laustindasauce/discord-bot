e7055ede4b85ce73d36bfc21a429b790994fc9dc
abspen1 <abspencer2097@gmail.com>
2020-12-23 12:10:54 -0700

    adding a way for users to see available levels

---
+++ functions/levels.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const embed = new Discord.MessageEmbed()
+
+module.exports = {
+    name: "level",
+    aliases: ['levels'],
+    description: "Displays embedded message with all available levels in the server.",
+    permLevel: "User",
+	execute(client) {
+		for (let i = client.config.permLevels.length; i > 0; i--) {
+            embed.addField(thisLevel.name, thisLevel, false)
+        }
+        return message.author.send(exampleEmbed)
+            .then(() => {
+                if (message.channel.type === 'dm') return;
+                message.reply('I\'ve sent you a DM with the available levels for this server!');
+            })
+            .catch(error => {
+                console.error(`Could not send levels DM to ${message.author.tag}.\n`, error);
+                message.reply('it seems like I can\'t DM you!');
+            });
+	}
+};
