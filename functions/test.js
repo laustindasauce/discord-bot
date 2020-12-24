c23946baa361af1bfe5f71e450b8f1f97147c00e
abspen1 <abspencer2097@gmail.com>
2020-12-24 09:25:29 -0700

    update test desc

--- functions/test.js
+++ functions/test.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: "test",
     aliases: ['testing', 'tests'],
-    description: "Tells you your permission level for the current message location.",
+    description: "Tells you your permission level for the current message location.  \`Only available in test environment\`",
     permLevel: "User",
     test: true,
     usage: '[all] OR [command name]'
