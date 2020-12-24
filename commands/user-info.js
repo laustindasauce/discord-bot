2c8c1f70790c5fb03bc308af1fc7a008d387a17e
abspen1 <abspencer2097@gmail.com>
2020-12-24 12:31:54 -0700

    add tag ability

--- commands/user-info.js
+++ commands/user-info.js
@@ -2,7 +2,21 @@ module.exports = {
 	name: 'user-info',
 	description: 'Display info about yourself.',
 	execute(message) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+		if (!message.mentions.users.size) {
+			console.log(message.author);
+			return message.author.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+		}
+
+		const info = message.mentions.users.map(user => {
+			console.log(user);
+			return `Their username: ${user.username}\nTheir ID: ${user.id}`;
+		});
+		
+		try {
+			message.channel.send(info);
+		} catch (error) {
+			console.error(error)
+		}
 	},
 	test() {
 		return true;
