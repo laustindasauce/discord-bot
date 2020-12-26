f02fe0d577a973be86c73838fc6eed2e8a1cd365
abspen1 <abspencer2097@gmail.com>
2020-12-26 13:41:50 -0700

    reorder the parameters

--- bot.js
+++ bot.js
@@ -136,7 +136,7 @@ client.on('message', message => {
 	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
 	try {
-		command.execute(redis, message, args, level);
+		command.execute(message, args, redis, level);
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
