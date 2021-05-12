7156c1f4f84b4b6bfaf6eafde13d22bf6ec1c446
Austin Spencer <abspencer2097@gmail.com>
2021-05-11 20:02:57 -0400

    Update some responses

--- events/message.js
+++ events/message.js
@@ -13,18 +13,17 @@ module.exports = {
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
 
-        const command = client.commands.get(commandName)
-            || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+        const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
 
         const level = client.permlevel(message);
 
         if (!command) return message.reply(`${args} is not a command!`);
         
-        if (command.readOnly) return message.reply(`Sorry, the command ${command.name} is read only!`);
+        if (command.readOnly) return message.reply(`Sorry, the command \`${command.name}\` is read only!`);
 
         if (command.guildOnly && message.channel.type === 'dm') {
-            return message.reply('I can\'t execute that command inside DMs!');
+            return message.reply(`I can\'t execute \`${command.name}\` inside DMs!`);
         }
 
         if (command.args && !args.length) {
@@ -37,9 +36,8 @@ module.exports = {
             return message.channel.send(reply);
         }
 
-        if (level < command.permLevel) {
-            return message.reply(`You don't have permissions to run ${command.name} command.`)
-        }
+        if (level < command.permLevel) return message.reply(`you don't have permissions to run \`${command.name}\` command.`)
+        
 
         if (!cooldowns.has(command.name)) {
             cooldowns.set(command.name, new Discord.Collection());
@@ -54,7 +52,7 @@ module.exports = {
 
             if (now < expirationTime) {
                 const timeLeft = (expirationTime - now) / 1000;
-                return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+                return message.reply(`please wait **${timeLeft.toFixed(1)}** more second(s) before reusing the \`${command.name}\` command.`);
             }
         }
 
@@ -65,7 +63,7 @@ module.exports = {
             command.execute(message, args, redis, level);
         } catch (error) {
             console.error(error);
-            message.reply('there was an error trying to execute that command!');
+            message.reply(`sorry, there was an error trying to execute \`${command.name}.\` \nPlease contact admin if issue persists.`);
         }
 	},
 	test() {
