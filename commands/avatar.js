6de6bffa79cf2e5e36b6638af248ef7f5c609f86
Austin Spencer <abspencer2097@gmail.com>
2021-10-08 23:40:43 -0700

    fix args

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,34 +1,42 @@
-const getUserFromMention = require('../utils/functions/getUserFromMention.js')
+const getUserFromMention = require("../utils/functions/getUserFromMention.js");
 
 module.exports = {
-	name: 'avatar',
-	aliases: ['icon', 'pfp'],
-	description: 'Get the avatar URL of the tagged user(s), or your own avatar.',
-	usage: '[*optional* @member]',
-	args: false,
-	readOnly: false,
-	channels: ['bot-testing'],
-	cooldown: 2,
-	permLevel: 0,
-	/**
-	 * This command is able to return the avatar of the user or a tagged member
-	 * 
-	 * @param {message Object} message the message Object that was sent to trigger this command
-	 * @param {array} _args the rest of the message after the command
-	 * @param {Redis client} _redis Redis client (our database)
-	 * @param {num} _level users permission level
-	 */
-	execute(message, _args, _redis, _level) {
-		if (args[0]) {
-			const user = getUserFromMention.execute(args[0], message.client);
-			if (!user) {
-				return message.reply('Please use a proper mention if you want to see someone elses avatar.');
-			}
-			return message.channel.send(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
-		}
-		return message.channel.send(`${message.author.username}, your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`);
-	},
-	test() {
-		return true;
-	},
+  name: "avatar",
+  aliases: ["icon", "pfp"],
+  description: "Get the avatar URL of the tagged user(s), or your own avatar.",
+  usage: "[*optional* @member]",
+  args: false,
+  readOnly: false,
+  channels: ["bot-testing"],
+  cooldown: 2,
+  permLevel: 0,
+  /**
+   * This command is able to return the avatar of the user or a tagged member
+   *
+   * @param {message Object} message the message Object that was sent to trigger this command
+   * @param {array} args the rest of the message after the command
+   * @param {Redis client} _redis Redis client (our database)
+   * @param {num} _level users permission level
+   */
+  execute(message, args, _redis, _level) {
+    if (args[0]) {
+      const user = getUserFromMention.execute(args[0], message.client);
+      if (!user) {
+        return message.reply(
+          "Please use a proper mention if you want to see someone elses avatar."
+        );
+      }
+      return message.channel.send(
+        `${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`
+      );
+    }
+    return message.channel.send(
+      `${
+        message.author.username
+      }, your avatar: ${message.author.displayAvatarURL({ dynamic: true })}`
+    );
+  },
+  test() {
+    return true;
+  },
 };
--- commands/kick.js
+++ commands/kick.js
@@ -1,66 +1,68 @@
-const getUserFromMention = require('../utils/functions/getUserFromMention.js')
+const getUserFromMention = require("../utils/functions/getUserFromMention.js");
 
 module.exports = {
-	name: 'kick',
-	description: 'Tag a member and kick them.',
-	usage: '[@member] reason the user needs kicked',
-	args: false,
-	readOnly: false,
-	guildOnly: true,
-	cooldown: 5,
-	permLevel: 3,
-	/**
-	 * This command is able to kick the tagged user
-	 * 
-	 * @param {message Object} message the message Object that was sent to trigger this command
-	 * @param {array} args the rest of the message after the command
-	 * @param {Redis client} _redis Redis client (our database)
-	 * @param {num} _level users permission level
-	 */
-	execute(message, args, _redis, _level) {
-		if (!message.mentions.users.size) {
-			return message.reply('you need to tag a user in order to kick them!');
-		}
+  name: "kick",
+  description: "Tag a member and kick them.",
+  usage: "[@member] reason the user needs kicked",
+  args: true,
+  readOnly: false,
+  guildOnly: true,
+  cooldown: 5,
+  permLevel: 3,
+  /**
+   * This command is able to kick the tagged user
+   *
+   * @param {message Object} message the message Object that was sent to trigger this command
+   * @param {array} args the rest of the message after the command
+   * @param {Redis client} _redis Redis client (our database)
+   * @param {num} _level users permission level
+   */
+  execute(message, args, _redis, _level) {
+    if (!message.mentions.users.size) {
+      return message.reply("you need to tag a user in order to kick them!");
+    }
 
-		const user = getUserFromMention.execute(args[0], message.client);
-		message.channel.send(`You wanted to kick: ${user.username}`);
-		
-		if (user) {
-			// Now we get the member from the user
-			const member = message.guild.member(user);
-			// Get the reason for the ban from the command
-            const reason = args.slice(1).join(' ');
-			// If the member is in the guild
-			if (member) {
-				/**
-				 * Kick the member
-				 * Make sure you run this on a member, not a user!
-				 * There are big differences between a user and a member
-				 */
-				member
-				.kick(reason)
-				.then(() => {
-					// We let the message author know we were able to kick the person
-					message.reply(`Successfully kicked ${member.displayName}`);
-				})
-				.catch(err => {
-					// An error happened
-					// This is generally due to the bot not being able to kick the member,
-					// either due to missing permissions or role hierarchy
-					message.reply(`I was unable to kick ${member.displayName}`);
-                    // Log the error
-                    console.error(err);
-				});
-			} else {
-				// The mentioned user isn't in this guild
-                message.reply(`${member.displayName} isn't in this guild!`);
-			}
-		} else {
-            // Otherwise, if no user was mentioned
-            return message.reply('Please use a proper mention if you want to ban someone.');
-        }
-	},
-	test() {
-		return true;
-	},
+    const user = getUserFromMention.execute(args[0], message.client);
+    message.channel.send(`You wanted to kick: ${user.username}`);
+
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // Get the reason for the ban from the command
+      const reason = args.slice(1).join(" ");
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick(reason)
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully kicked ${member.displayName}`);
+          })
+          .catch((err) => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply(`I was unable to kick ${member.displayName}`);
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply(`${member.displayName} isn't in this guild!`);
+      }
+    } else {
+      // Otherwise, if no user was mentioned
+      return message.reply(
+        "Please use a proper mention if you want to ban someone."
+      );
+    }
+  },
+  test() {
+    return true;
+  },
 };
